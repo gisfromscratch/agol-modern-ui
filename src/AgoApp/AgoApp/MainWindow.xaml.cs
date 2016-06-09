d55547e44ce1e07e1f1a9a7834375ef47be12d84
J.T <gisfromscratch@live.de>
2016-06-09 22:01:50 +0200

    Added the mahapps metro and mvvmllight nuget packages.

---
+++ src/AgoApp/AgoApp/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MahApps.Metro.Controls;
+
+namespace AgoApp
+{
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
